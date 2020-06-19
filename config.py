9ccee8019106ce14e02db00bbc7b63889be025e1
islomferg <52602110+islomferg@users.noreply.github.com>
2020-06-20 00:29:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 apiUrl = 'https://verylegit.link/sketchify'
-botToken = '1128791159:AAG1-hNOIcapahqVa53AtZzxdRb0Ra-SQuY' # Bot token (@botfather)
+botToken = '1128791759:AAG1-hNOIcapahqVa53AtZzxdRb0Ra-SQuY' # Bot token (@botfather)
